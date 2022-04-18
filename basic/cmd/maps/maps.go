8f2ce9fbb37648e899545c5570bb73866724cc4e
kyu08 <kyu08@users.noreply.github.com>
2022-04-19 03:15:58 +0900

    wip

--- basic/cmd/maps/maps.go
+++ basic/cmd/maps/maps.go
@@ -4,12 +4,25 @@ import "errors"
 
 type Dictionary map[string]string
 
+var (
+	ErrNotFound   = errors.New("ERR: value not exist.")
+	ErrWordExists = errors.New("ERR: cannot add word because it already exists.")
+)
+
 func (d Dictionary) Search(key string) (string, error) {
-	var result string = d[key]
-	var err error
-	if result == "" {
-		err = errors.New("ERR: value not exist.")
+	result, ok := d[key]
+	if !ok {
+		return "", ErrNotFound
 	}
 
-	return result, err
+	return result, nil
+}
+
+func (d Dictionary) Add(key, value string) error {
+	_, err := d.Search(key)
+	if err == nil {
+		return ErrWordExists
+	}
+	d[key] = value
+	return nil
 }
--- basic/test/map_test.go
+++ basic/test/map_test.go
@@ -15,8 +15,58 @@ func TestSearch(t *testing.T) {
 	t.Run("search non-existing value", func(t *testing.T) {
 		dictionary := maps.Dictionary{"test": "test dayo"}
 		_, err := dictionary.Search("nekonekoneko")
-		assertError(t, err)
+		assertError(t, err, maps.ErrNotFound)
 	})
+}
+
+
+
+
+
+
+
+
+
+
+
+`map_test.go:18: 👺 got ERR: value not exist. but want ERR: value not exist.`
+ってなるってことはもしかして error 同士の比較ってうまくできないのかな？
+
+
+
+
+
+
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := maps.Dictionary{}
+		key := "editor"
+		value := "vim"
+		dictionary.Add(key, value)
+		assertDefinition(t, dictionary, key, value)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		dictionary := maps.Dictionary{"one": "ichi"}
+		err := dictionary.Add("one", "uno")
+		if err != nil {
+			t.Errorf("👺 got %s but want %s", err, maps.ErrWordExists)
+		}
+	})
+}
+
+// helper
+func assertDefinition(t *testing.T, dictionary maps.Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("👺 should find added value: ", err)
+	}
+	if got != definition {
+		t.Errorf("👺 got %q but want %q", got, definition)
+	}
 }
 
 func assertStrings(t *testing.T, got, want string) {
@@ -26,9 +76,16 @@ func assertStrings(t *testing.T, got, want string) {
 	}
 }
 
-func assertError(t *testing.T, got error) {
+func assertError(t *testing.T, got error, want error) {
 	t.Helper()
+	if got != nil {
+		t.Errorf("👺 got %s but want %s", got, want)
+		return
+	}
 	if got == nil {
-		t.Errorf("👺 got nil but want error")
+		if want == nil {
+			return
+		}
+		t.Errorf("👺 got nil but want %s", want)
 	}
 }
