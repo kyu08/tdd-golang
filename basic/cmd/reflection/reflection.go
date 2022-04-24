6d719416545bbc3cca15b1a86226af3594b3eb9e
kyu08 <kyu08@users.noreply.github.com>
2022-04-25 01:00:43 +0900

    update

--- basic/cmd/reflection/reflection.go
+++ basic/cmd/reflection/reflection.go
@@ -5,7 +5,34 @@ import (
 )
 
 func Walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+	var numberOfValues = 0
+	var getField func(int) reflect.Value
+
+	switch val.Kind() {
+	case reflect.Slice, reflect.Array:
+		numberOfValues = val.Len()
+		getField = val.Index
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Map:
+		for _, k := range val.MapKeys() {
+			Walk(val.MapIndex(k).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		Walk(getField(i).Interface(), fn)
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	return val
 }
--- basic/test/reflection_test.go
+++ basic/test/reflection_test.go
@@ -6,30 +6,94 @@ import (
 	"testing"
 )
 
-func TestWalk(t *testing.T) {
+type Person struct {
+	Name Name
+	City string
+}
 
+type Name struct {
+	First string
+	Last  string
+}
+
+func TestWalk(t *testing.T) {
 	tests := []struct {
-		name           string
-		x              interface{}
-		expectedValues []string
+		Name           string
+		X              interface{}
+		ExpectedValues []string
 	}{
 		{
 			"one string field",
 			struct {
-				name string
+				Name string
 			}{"john"},
 			[]string{"john"},
+		},
+		{
+			"struct with two string fields",
+			struct {
+				FirstName string
+				LastName  string
+			}{"john", "tanaka"},
+			[]string{"john", "tanaka"},
+		},
+		{
+			"nested struct",
+			Person{
+				Name: Name{"john", "tanaka"},
+				City: "tokyo",
+			},
+			[]string{"john", "tanaka", "tokyo"},
+		},
+		{
+			"pointers to things",
+			&Person{
+				Name: Name{"john", "tanaka"},
+				City: "tokyo",
+			},
+			[]string{"john", "tanaka", "tokyo"},
+		},
+		{
+			"slice",
+			[]Person{
+				{Name: Name{"john", "tanaka"},
+					City: "tokyo",
+				},
+				{Name: Name{"u", "ishiguro"},
+					City: "kyoto",
+				},
+			},
+			[]string{"john", "tanaka", "tokyo", "u", "ishiguro", "kyoto"},
+		},
+		{
+			"array",
+			[2]Person{
+				{Name: Name{"john", "tanaka"},
+					City: "tokyo",
+				},
+				{Name: Name{"u", "ishiguro"},
+					City: "kyoto",
+				},
+			},
+			[]string{"john", "tanaka", "tokyo", "u", "ishiguro", "kyoto"},
+		},
+		{
+			"map",
+			map[string]string{
+				"one": "ichi", "two": "ni",
+			},
+			[]string{"ichi", "ni"},
 		},
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.Name, func(t *testing.T) {
 			var got []string
-			reflection.Walk(tt.x, func(input string) {
+			reflection.Walk(tt.X, func(input string) {
 				got = append(got, input)
 			})
-			if !reflect.DeepEqual(got, tt.expectedValues) {
-				t.Errorf("got %v, want %v", got, tt.expectedValues)
+			if !reflect.DeepEqual(got, tt.ExpectedValues) {
+				t.Errorf("👺 got %v, want %v", got, tt.ExpectedValues)
 			}
 		})
 	}
