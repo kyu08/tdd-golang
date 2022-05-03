e2c0fd028aba3a67fc89f4abe138e6dc65da6a45
kyu08 <kyu08@users.noreply.github.com>
2022-05-03 19:19:20 +0900

    refactor

--- basic/cmd/roman_numerals/roman_numerals.go
+++ basic/cmd/roman_numerals/roman_numerals.go
@@ -5,23 +5,10 @@ import "strings"
 func ConvertRoman(arabic int) string {
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic > 9:
-			result.WriteString("X")
-			arabic -= 10
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic -= 1
+	for _, n := range allRomanNumerals {
+		for arabic >= n.value {
+			result.WriteString(n.symbol)
+			arabic -= n.value
 		}
 	}
 
