5f2a8eb86be6a73d405bd4a14b4e778c7e99d4ab
kyu08 <kyu08@users.noreply.github.com>
2022-05-03 18:56:20 +0900

    4

--- basic/cmd/roman_numerals/roman_numerals.go
+++ basic/cmd/roman_numerals/roman_numerals.go
@@ -3,12 +3,13 @@ package roman_numerals
 import "strings"
 
 func ConvertRoman(arabic int) string {
-	if arabic == 4 {
-		return "IV"
-	}
 
 	var result strings.Builder
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if arabic == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 	return result.String()
