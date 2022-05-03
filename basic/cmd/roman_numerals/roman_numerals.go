17a19adfc771219dc04584bf60ba9c66de1b6584
kyu08 <kyu08@users.noreply.github.com>
2022-05-03 18:53:47 +0900

    1~4

--- basic/cmd/roman_numerals/roman_numerals.go
+++ basic/cmd/roman_numerals/roman_numerals.go
@@ -1,5 +1,15 @@
 package roman_numerals
 
-func ConvertRoman(numeral int) string {
-	return "I"
+import "strings"
+
+func ConvertRoman(arabic int) string {
+	if arabic == 4 {
+		return "IV"
+	}
+
+	var result strings.Builder
+	for i := 0; i < arabic; i++ {
+		result.WriteString("I")
+	}
+	return result.String()
 }
--- basic/cmd/roman_numerals/roman_numerals_test.go
+++ basic/cmd/roman_numerals/roman_numerals_test.go
@@ -3,10 +3,23 @@ package roman_numerals
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertRoman(1)
-	want := "I"
+	tests := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV", 4, "IV"},
+	}
 
-	if got != want {
-		t.Errorf("👺 got %q, but want %q", got, want)
+	for _, tt := range tests {
+		t.Run(tt.Description, func(t *testing.T) {
+			got := ConvertRoman(tt.Arabic)
+			if got != tt.Want {
+				t.Errorf("👺 got %q, but want %q", got, tt.Want)
+			}
+		})
 	}
 }
