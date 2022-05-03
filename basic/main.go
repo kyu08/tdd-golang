8a98dae7686dc78464b9824f01c8f89bca5a530e
kyu08 <kyu08@users.noreply.github.com>
2022-05-03 16:23:07 +0900

    value tips

--- basic/main.go
+++ basic/main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
+	const a = iota
 
-	x := struct {
-		name string
-		ok   bool
-	}{"a", false}
+	fmt.Print(a)
+	fmt.Print(a)
+	fmt.Print(a)
+	// x := struct {
+	// 	name string
+	// 	ok   bool
+	// }{"a", false}
 
-	val := reflect.ValueOf(x)
-	fmt.Printf("numFiled: %d\n", val.NumField())
-	fmt.Printf("field(0): %v\n", val.Field(0))
-	fmt.Printf("field(1): %v", val.Field(1))
+	// val := reflect.ValueOf(x)
+	// fmt.Printf("numFiled: %d\n", val.NumField())
+	// fmt.Printf("field(0): %v\n", val.Field(0))
+	// fmt.Printf("field(1): %v", val.Field(1))
 }
